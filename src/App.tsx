4e43ece3a78af05c0484e0b2ebe82bd2bdb2b6e5
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-08-10 07:08:24 +0000

    add 普通のフォーム

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
+import { useState } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+
+  const handleSubmit = () => {
+    console.log("名前:", name);
+    console.log("性別:", gender);
+  };
 
   return (
     <>
-      Hello World
+      <h1>フォーム</h1>
+      <div>
+        <label>
+          名前:
+          <input
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        性別:
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={gender === "男性"}
+            onChange={e => setGender(e.target.value)}
+          />
+          男性
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={gender === "女性"}
+            onChange={e => setGender(e.target.value)}
+          />
+          女性
+        </label>
+      </div>
+      <button type="button" onClick={handleSubmit}>
+        送信
+      </button>
     </>
-  )
+  );
 }
 
 export default App
